7f495bbfb5330d08ec1c5a2104887b4aa7bb8129
Kenny <kbarron525@gmail.com>
2012-07-31 13:40:40 -0500

    Initial Commit

---
+++ HelloApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloApp
+//
+//  Created by iOS Camp on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloApp
+//
+//  Created by iOS Camp on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
